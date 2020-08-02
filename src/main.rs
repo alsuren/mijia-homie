e97bdc83140fb5b5516ea6b409142dba10267322
David Laban <alsuren@gmail.com>
2020-08-02 15:37:04 +0100

    try the version in their examples dir?

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,40 @@
-use rumqttc::{EventLoop, MqttOptions, Request};
+use async_channel::Sender;
+use tokio::{task, time};
+
+use rumqttc::{self, EventLoop, MqttOptions, Publish, QoS, Request, Subscribe};
 use std::error::Error;
 use std::time::Duration;
 
 #[tokio::main(core_threads = 1)]
-async fn main() {
-    let mut mqttoptions = MqttOptions::new("rumqtt-async", "test.mosquitto.org", 1883);
-    let requests_rx = tokio::stream::iter(Vec::new());
-    let mut eventloop = EventLoop::new(mqttoptions, requests_rx).await;
+async fn main() -> Result<(), Box<dyn Error>> {
+    pretty_env_logger::init();
+    color_backtrace::install();
+
+    let mut mqttoptions = MqttOptions::new("test-1", "localhost", 1883);
+    mqttoptions.set_keep_alive(5);
+
+    let mut eventloop = EventLoop::new(mqttoptions, 10).await;
+    let requests_tx = eventloop.handle();
+    task::spawn(async move {
+        requests(requests_tx).await;
+        time::delay_for(Duration::from_secs(3)).await;
+    });
 
     loop {
-        let notification = eventloop.poll().await.unwrap();
-        println!("Received = {:?}", notification);
-        tokio::time::delay_for(Duration::from_secs(1)).await;
+        let (incoming, outgoing) = eventloop.poll().await?;
+        println!("Incoming = {:?}, Outgoing = {:?}", incoming, outgoing);
     }
+}
+
+async fn requests(requests_tx: Sender<Request>) {
+    let subscription = Subscribe::new("hello/world", QoS::AtMostOnce);
+    let _ = requests_tx.send(Request::Subscribe(subscription)).await;
+
+    for i in 0..10 {
+        let publish = Publish::new("hello/world", QoS::AtLeastOnce, vec![i; i as usize]);
+        requests_tx.send(Request::Publish(publish)).await.unwrap();
+        time::delay_for(Duration::from_secs(1)).await;
+    }
+
+    time::delay_for(Duration::from_secs(120)).await;
 }
