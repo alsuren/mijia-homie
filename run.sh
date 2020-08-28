alsuren/mijia-homie

commit 2c4f3f5831a571b0cbe30adcb978cc6a7a5fe580

Support building for aarch64 too.

run.sh
@@ -5,23 +5,24 @@ set -euxo pipefail
 #
 #     cargo install --git=https://github.com/alsuren/cross --branch=docker-build-context
 #
+TARGET=${TARGET:-armv7-unknown-linux-gnueabi}
 TARGET_SSH=${TARGET_SSH:-pi@raspberrypi.local}
 PROFILE=${PROFILE:-debug}
 RUN=${RUN:-1}
 
 if [ $PROFILE = release ]
 then
-    time cross build --target armv7-unknown-linux-gnueabihf --release
+    time cross build --target $TARGET --release
 elif [ $PROFILE = debug ]
 then
-    time cross build --target armv7-unknown-linux-gnueabihf
+    time cross build --target $TARGET --verbose
 else
     echo "Invalid profile '$PROFILE'"
     exit 1
 fi
 
-time rsync --progress target/armv7-unknown-linux-gnueabihf/$PROFILE/read-all-devices $TARGET_SSH:read-all-devices-next
-time rsync --progress target/armv7-unknown-linux-gnueabihf/$PROFILE/publish-mqtt $TARGET_SSH:publish-mqtt
+time rsync --progress target/$TARGET/$PROFILE/read-all-devices $TARGET_SSH:read-all-devices-next
+time rsync --progress target/$TARGET/$PROFILE/publish-mqtt $TARGET_SSH:publish-mqtt
 
 if [ $RUN -eq 1 ]
 then

Cross.toml
@@ -9,3 +9,9 @@
 # will be able to use `context` and `dockerfile` to achieve the same result.
 context = "./docker"
 dockerfile = "./docker/Dockerfile.buster-gst-armv7-unknown-linux-gnueabihf"
+
+[target.aarch64-unknown-linux-gnu]
+# image = "alsuren/xiaomi-temp-aarch64-builder"
+
+context = "./docker"
+dockerfile = "./docker/Dockerfile.buster-gst-aarch64-unknown-linux-gnu"

docker/Dockerfile.buster-gst-aarch64-unknown-linux-gnu
@@ -1,0 +1,29 @@
+FROM alsuren/cross-context:latest as context
+
+FROM debian:buster
+
+COPY  --from=context common.sh /
+RUN /common.sh
+
+COPY  --from=context cmake.sh /
+RUN /cmake.sh
+
+COPY  --from=context xargo.sh /
+RUN /xargo.sh
+
+RUN apt-get install --assume-yes --no-install-recommends \
+    g++-aarch64-linux-gnu \
+    libc6-dev-arm64-cross
+
+RUN dpkg --add-architecture arm64 && \
+    apt-get update && \
+    apt-get install -y libgstreamer1.0-dev:arm64 libgstreamer-plugins-base1.0-dev:arm64 libssl-dev:arm64
+
+RUN apt-get install -y libdbus-1-dev:arm64
+
+ENV CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc \
+    CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc \
+    CXX_aarch64_unknown_linux_gnu=aarch64-linux-gnu-g++ \
+    QEMU_LD_PREFIX=/usr/aarch64-linux-gnu \
+    RUST_TEST_THREADS=1 \
+    PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig

commit c5e42426fc493a8a19f8cf62693cefe8153b6833

Fix bugs introduced in 2c4f3f5831a571b0cbe30adcb978cc6a7a5fe580.

run.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 #
 #     cargo install --git=https://github.com/alsuren/cross --branch=docker-build-context
 #
-TARGET=${TARGET:-armv7-unknown-linux-gnueabi}
+TARGET=${TARGET:-armv7-unknown-linux-gnueabihf}
 TARGET_SSH=${TARGET_SSH:-pi@raspberrypi.local}
 PROFILE=${PROFILE:-debug}
 RUN=${RUN:-1}
@@ -15,7 +15,7 @@ then
     time cross build --target $TARGET --release
 elif [ $PROFILE = debug ]
 then
-    time cross build --target $TARGET --verbose
+    time cross build --target $TARGET
 else
     echo "Invalid profile '$PROFILE'"
     exit 1