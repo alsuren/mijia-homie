8d11302ac24e0531eb2acdb2b85343d76899caf9
David Laban <alsuren@gmail.com>
2020-08-31 12:34:03 +0100

    shfmt -w -bn -i 4 **/*.sh

--- bluez-generated/introspect.sh
+++ bluez-generated/introspect.sh
@@ -6,38 +6,34 @@ set -euxo pipefail
 
 cd "$(dirname "$0")"
 
-
 ssh pi@raspberrypi.local \
     gdbus introspect --system --dest org.bluez --object-path / --recurse \
     | grep -E '^ *(node|interface) .* {$' \
     | (
-    declare -A interface_to_path
+        declare -A interface_to_path
 
-    while read keyword value _bracket
-    do
-        if [ $keyword = 'node' ]
-        then
-            current_path=$value
-        elif [ $keyword = 'interface' ]
-        then
-            interface_to_path[${value}]=$current_path
-            echo ${interface_to_path[${value}]}
-        else
-            echo "unexpected line $keyword $value $_bracket"
-            exit 1
-        fi
-    done
+        while read keyword value _bracket; do
+            if [ $keyword = 'node' ]; then
+                current_path=$value
+            elif [ $keyword = 'interface' ]; then
+                interface_to_path[${value}]=$current_path
+                echo ${interface_to_path[${value}]}
+            else
+                echo "unexpected line $keyword $value $_bracket"
+                exit 1
+            fi
+        done
 
-    for interface in ${!interface_to_path[@]}; do
-        echo $interface -- ${interface_to_path[${interface}]}
-        ssh pi@raspberrypi.local \
-            gdbus introspect \
+        for interface in ${!interface_to_path[@]}; do
+            echo $interface -- ${interface_to_path[${interface}]}
+            ssh pi@raspberrypi.local \
+                gdbus introspect \
                 --system \
                 --dest=org.bluez \
                 --object-path=${interface_to_path[${interface}]} \
                 --xml \
-            | xmllint --format - \
-            | grep -v '^ *<node name=".*"/>$' \
-            > specs/$interface.xml
-    done
-)
+                | xmllint --format - \
+                | grep -v '^ *<node name=".*"/>$' \
+                    >specs/$interface.xml
+        done
+    )
