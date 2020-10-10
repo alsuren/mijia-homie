35634d533c44d5540328eb7602edbd601fe1e448
Andrew Walbran <qwandor@google.com>
2020-10-10 19:12:36 +0100

    Example to discover sensors.

---
+++ mijia/examples/list-sensors.rs
@@ -0,0 +1,17 @@
+use mijia::MijiaSession;
+
+#[tokio::main]
+async fn main() -> Result<(), eyre::Error> {
+    let (dbus_handle, session) = MijiaSession::new().await?;
+
+    session.bt_session.start_discovery().await?;
+
+    let sensors = session.get_sensors().await?;
+    println!("Sensors:");
+    for sensor in sensors {
+        println!("{}: {:?}", sensor.mac_address, sensor.id);
+    }
+
+    dbus_handle.await?;
+    Ok(())
+}
