dd2337602efb615ef931d259e5d611e158052d2a
Andrew Walbran <qwandor@google.com>
2020-10-10 23:49:32 +0100

    Wait for a few seconds after starting to scan.

--- mijia/examples/list-sensors.rs
+++ mijia/examples/list-sensors.rs
@@ -1,4 +1,8 @@
 use mijia::MijiaSession;
+use std::time::Duration;
+use tokio::time;
+
+const SCAN_DURATION: Duration = Duration::from_secs(5);
 
 #[tokio::main]
 async fn main() -> Result<(), eyre::Error> {
@@ -6,8 +10,11 @@ async fn main() -> Result<(), eyre::Error> {
 
     let (_, session) = MijiaSession::new().await?;
 
+    // Start scanning for Bluetooth devices, and wait a while for some to be discovered.
     session.bt_session.start_discovery().await?;
+    time::delay_for(SCAN_DURATION).await;
 
+    // Get the list of sensors which are currently known and print them.
     let sensors = session.get_sensors().await?;
     println!("Sensors:");
     for sensor in sensors {
