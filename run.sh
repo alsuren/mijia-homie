ccb61bce3b405fff519a45d288c4a86f3da8a73c
David Laban <alsuren@gmail.com>
2020-10-12 22:09:32 +0100

    EXAMPLE=list-sensors run.sh

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@ TARGET_SSH=${TARGET_SSH:-pi@raspberrypi.local}
 PROFILE=${PROFILE:-debug}
 RUN=${RUN:-1}
 USE_SYSTEMD=${USE_SYSTEMD:-1}
+EXAMPLE=${EXAMPLE:-}
 
 if [ $PROFILE = release ]; then
     PROFILE_FLAG=--release
@@ -20,14 +21,18 @@ else
     exit 1
 fi
 
-time cross build $PROFILE_FLAG --target $TARGET --bin publish-mqtt
-time cross build $PROFILE_FLAG --target $TARGET --example list-sensors
-
-time rsync --progress target/$TARGET/$PROFILE/publish-mqtt $TARGET_SSH:publish-mqtt
-time rsync --progress target/$TARGET/$PROFILE/examples/list-sensors $TARGET_SSH:list-sensors
+if [ "${EXAMPLE}" != "" ]; then
+    time cross build $PROFILE_FLAG --target $TARGET --example $EXAMPLE
+    time rsync --progress target/$TARGET/$PROFILE/examples/$EXAMPLE $TARGET_SSH:$EXAMPLE
+else
+    time cross build $PROFILE_FLAG --target $TARGET --bin publish-mqtt
+    time rsync --progress target/$TARGET/$PROFILE/publish-mqtt $TARGET_SSH:publish-mqtt
+fi
 
 if [ $RUN = 1 ]; then
-    if [ $USE_SYSTEMD = 1 ]; then
+    if [  $EXAMPLE != "" ]; then
+        ssh $TARGET_SSH ./$EXAMPLE
+    elif [ $USE_SYSTEMD = 1 ]; then
         scp publish-mqtt.service $TARGET_SSH:publish-mqtt.service
         ssh $TARGET_SSH sudo mv publish-mqtt.service /etc/systemd/system/publish-mqtt.service
         ssh $TARGET_SSH sudo systemctl daemon-reload
