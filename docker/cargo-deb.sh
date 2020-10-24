e41979195f64a4e11f877efdf28e2badff3a6a6a
Andrew Walbran <qwandor@google.com>
2020-10-24 18:21:11 +0100

    Add cargo-deb to Dockerfile.

---
+++ docker/cargo-deb.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -x
+set -euo pipefail
+
+main() {
+    local dependencies=(
+        ca-certificates
+        curl
+    )
+
+    apt-get update
+    local purge_list=()
+    for dep in "${dependencies[@]}"; do
+        if ! dpkg -L "${dep}"; then
+            apt-get install --assume-yes --no-install-recommends "${dep}"
+            purge_list+=( "${dep}" )
+        fi
+    done
+
+    export RUSTUP_HOME=/tmp/rustup
+    export CARGO_HOME=/tmp/cargo
+
+    curl --retry 3 -sSfL https://sh.rustup.rs -o rustup-init.sh
+    sh rustup-init.sh -y --no-modify-path --profile minimal
+    rm rustup-init.sh
+
+    PATH="${CARGO_HOME}/bin:${PATH}" cargo install cargo-deb --root /usr/local
+
+    rm -r "${RUSTUP_HOME}" "${CARGO_HOME}"
+
+    if (( ${#purge_list[@]} )); then
+      apt-get purge --assume-yes --auto-remove "${purge_list[@]}"
+    fi
+
+    rm "${0}"
+}
+
+main "${@}"
