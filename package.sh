0be066800bec70efc53d356652748b386e739d25
Andrew Walbran <qwandor@google.com>
2020-10-24 20:35:40 +0100

    Work around cargo-deb bug.

--- package.sh
+++ package.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 cross build --release --target armv7-unknown-linux-gnueabihf --bin mijia-homie
-cargo deb -p mijia-homie --target armv7-unknown-linux-gnueabihf --no-build
 
-cargo deb -p mijia-homie
+cd mijia-homie
+cargo deb --target armv7-unknown-linux-gnueabihf --no-build
+cargo deb
