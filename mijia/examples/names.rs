7a5038fa6669b59f1646728a7296a1db72490cdb
David Laban <alsuren@gmail.com>
2020-12-30 15:48:09 +0000

    names example compiles

--- mijia/examples/names.rs
+++ mijia/examples/names.rs
@@ -1,22 +1,23 @@
 //! Example of how to subscribe to readings from one or more sensors.
 
 use eyre::Report;
-use mijia::{MijiaEvent, MijiaSession, SensorProps};
-use std::process::exit;
+use mijia::{MijiaSession, SensorProps};
+use std::collections::HashMap;
+use std::io::Write;
 use std::time::Duration;
-use tokio::stream::StreamExt;
+use std::{io::stdin, process::exit};
 use tokio::time;
 
 const SCAN_DURATION: Duration = Duration::from_secs(5);
+const SENSOR_NAMES_FILE: &str = "sensor-names.toml";
 
 #[tokio::main]
 async fn main() -> Result<(), Report> {
     pretty_env_logger::init();
 
-    let filters = parse_args()?;
+    let excludes = get_known_sensors()?;
 
     let (_, session) = MijiaSession::new().await?;
-    let mut events = session.event_stream().await?;
 
     // Start scanning for Bluetooth devices, and wait a while for some to be discovered.
     session.bt_session.start_discovery().await?;
@@ -28,52 +29,59 @@ async fn main() -> Result<(), Report> {
     println!("Sensors:");
     for sensor in sensors
         .iter()
-        .filter(|sensor| should_include_sensor(sensor, &filters))
+        .filter(|sensor| should_include_sensor(sensor, &excludes))
     {
         println!("Connecting to {} ({:?})", sensor.mac_address, sensor.id);
         if let Err(e) = session.bt_session.connect(&sensor.id).await {
             println!("Failed to connect to {}: {:?}", sensor.mac_address, e);
             continue;
         }
-        session.start_notify_sensor(&sensor.id).await?;
-    }
 
-    println!("Readings:");
-    while let Some(event) = events.next().await {
-        match event {
-            MijiaEvent::Readings { id, readings } => {
-                println!("{:?}: {}", id, readings);
-            }
-            _ => println!("Event: {:?}", event),
+        {
+            let sensor = sensor.clone();
+
+            tokio::task::spawn_blocking(move || {
+                use std::fs::OpenOptions;
+                println!(
+                    "What name do you want to use for {} ({:?})?",
+                    sensor.mac_address, sensor.id
+                );
+                let mut name = String::new();
+                stdin().read_line(&mut name)?;
+
+                let mut file = OpenOptions::new().append(true).open(SENSOR_NAMES_FILE)?;
+                writeln!(
+                    file,
+                    r#""{mac}" = "{name}""#,
+                    mac = sensor.mac_address,
+                    name = name
+                )?;
+                Ok::<_, Report>(())
+            })
+            .await??;
         }
+
+        session.bt_session.disconnect(&sensor.id).await?;
     }
 
     Ok(())
 }
 
-fn parse_args() -> Result<Vec<String>, Report> {
-    // If at least one command-line argument is given, we will only try to connect to sensors whose
-    // MAC address containts one of them as a sub-string.
-    let mut args = std::env::args();
-    let binary_name = args
-        .next()
-        .ok_or_else(|| eyre::eyre!("Binary name missing"))?;
-    let filters: Vec<_> = args.collect();
+fn get_known_sensors() -> Result<Vec<String>, Report> {
+    let names = toml::from_str::<HashMap<String, String>>(SENSOR_NAMES_FILE).unwrap_or_default();
 
-    if filters
-        .iter()
+    if names
+        .keys()
         .any(|f| f.contains(|c: char| !(c.is_ascii_hexdigit() || c == ':')))
     {
-        eprintln!("Invalid MAC addresses {:?}", filters);
-        eprintln!("Usage:");
-        eprintln!("  {} [MAC address]...", binary_name);
+        eprintln!("Invalid MAC addresses {:?}", names);
         exit(1);
     }
 
-    Ok(filters)
+    Ok(names.keys().cloned().collect())
 }
 
-fn should_include_sensor(sensor: &SensorProps, filters: &Vec<String>) -> bool {
+fn should_include_sensor(sensor: &SensorProps, excludes: &Vec<String>) -> bool {
     let mac = sensor.mac_address.to_string();
-    filters.is_empty() || filters.iter().any(|filter| mac.contains(filter))
+    !excludes.iter().any(|filter| mac.contains(filter))
 }
