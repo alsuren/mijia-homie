20001c8c6687cadd107bcd1659e372681069f99f
Andrew Walbran <qwandor@google.com>
2021-01-03 15:17:38 +0000

    No point scanning in characteristic example.

--- mijia/examples/characteristics.rs
+++ mijia/examples/characteristics.rs
@@ -1,18 +1,10 @@
 use mijia::bluetooth::BluetoothSession;
-use std::time::Duration;
-use tokio::time;
-
-const SCAN_DURATION: Duration = Duration::from_secs(5);
 
 #[tokio::main]
 async fn main() -> Result<(), eyre::Report> {
     pretty_env_logger::init();
 
     let (_, session) = BluetoothSession::new().await?;
-
-    // Start scanning for Bluetooth devices, and wait a while for some to be discovered.
-    session.start_discovery().await?;
-    time::sleep(SCAN_DURATION).await;
 
     // Get the list of devices whose services are currently known and print them with their
     // characteristics.
