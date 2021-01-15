2f7184adaa38c85ee5cede165b6a0da2e0832f65
Andrew Walbran <qwandor@google.com>
2021-01-15 18:48:17 +0000

    Using cross version for docker images rather than "latest".

--- docker/update.sh
+++ docker/update.sh
@@ -2,13 +2,15 @@
 
 set -euo pipefail
 
-docker build ./docker -f docker/Dockerfile.debian-buster-aarch64 \
-    -t ghcr.io/qwandor/cross-dbus-debian-buster-aarch64:latest
-docker build ./docker -f docker/Dockerfile.debian-buster-armv7 \
-    -t ghcr.io/qwandor/cross-dbus-debian-buster-armv7:latest
-docker build ./docker -f docker/Dockerfile.debian-buster-x86_64 \
-    -t ghcr.io/qwandor/cross-dbus-debian-buster-x86_64:latest
+VERSION=0.2.1
 
-docker push ghcr.io/qwandor/cross-dbus-debian-buster-aarch64:latest
-docker push ghcr.io/qwandor/cross-dbus-debian-buster-armv7:latest
-docker push ghcr.io/qwandor/cross-dbus-debian-buster-x86_64:latest
+docker build ./docker -f docker/Dockerfile.debian-buster-aarch64 \
+    -t ghcr.io/qwandor/cross-dbus-debian-buster-aarch64:$VERSION
+docker build ./docker -f docker/Dockerfile.debian-buster-armv7 \
+    -t ghcr.io/qwandor/cross-dbus-debian-buster-armv7:$VERSION
+docker build ./docker -f docker/Dockerfile.debian-buster-x86_64 \
+    -t ghcr.io/qwandor/cross-dbus-debian-buster-x86_64:$VERSION
+
+docker push ghcr.io/qwandor/cross-dbus-debian-buster-aarch64:$VERSION
+docker push ghcr.io/qwandor/cross-dbus-debian-buster-armv7:$VERSION
+docker push ghcr.io/qwandor/cross-dbus-debian-buster-x86_64:$VERSION
