b816346da0df779d78977b47df2f23a585f9a97e
David Laban <alsuren@gmail.com>
2021-01-31 18:20:05 +0000

    reword comment

--- mijia-homie/src/mijia-names.rs
+++ mijia-homie/src/mijia-names.rs
@@ -33,8 +33,8 @@ async fn main() -> Result<(), Report> {
     session.bt_session.start_discovery().await?;
     time::sleep(SCAN_DURATION).await;
 
-    // Get the list of sensors which are currently known, connect those which
-    // are not already known about.
+    // Get the list of sensors which are currently visible, connect those which
+    // are not already named.
     let sensors = session.get_sensors().await?;
     for sensor in sensors
         .iter()
