1235805679710c5a07ff43f344176d2a9c9f3127
Andrew Walbran <qwandor@google.com>
2025-09-12 12:32:01 +0100

    Fix cross builds.
    aws-lc-sys was failing to build in cross for some architectures due to
    missing bindgen-cli. Adding it to the Docker containers required
    updating from Debian buster to trixie, as buster is no longer supported.

--- docker/update.sh
+++ docker/update.sh
@@ -2,17 +2,18 @@
 
 set -euo pipefail
 
-VERSION=0.2.1-1
+VERSION=0.2.1-2
 
-docker build ./docker -f docker/Dockerfile.debian-buster-aarch64 \
-    -t ghcr.io/qwandor/cross-dbus-debian-buster-aarch64:$VERSION
-docker build ./docker -f docker/Dockerfile.debian-buster-armv7 \
-    -t ghcr.io/qwandor/cross-dbus-debian-buster-armv7:$VERSION
-docker build ./docker -f docker/Dockerfile.debian-buster-armv6 \
-    -t ghcr.io/qwandor/cross-dbus-debian-buster-armv6:$VERSION
-docker build ./docker -f docker/Dockerfile.debian-buster-x86_64 \
-    -t ghcr.io/qwandor/cross-dbus-debian-buster-x86_64:$VERSION
+docker build ./docker -f docker/Dockerfile.debian-trixie-aarch64 \
+    -t ghcr.io/qwandor/cross-dbus-debian-trixie-aarch64:$VERSION
+docker build ./docker -f docker/Dockerfile.debian-trixie-armv7 \
+    -t ghcr.io/qwandor/cross-dbus-debian-trixie-armv7:$VERSION
+docker build ./docker -f docker/Dockerfile.debian-trixie-armv6 \
+    -t ghcr.io/qwandor/cross-dbus-debian-trixie-armv6:$VERSION
+docker build ./docker -f docker/Dockerfile.debian-trixie-x86_64 \
+    -t ghcr.io/qwandor/cross-dbus-debian-trixie-x86_64:$VERSION
 
-docker push ghcr.io/qwandor/cross-dbus-debian-buster-aarch64:$VERSION
-docker push ghcr.io/qwandor/cross-dbus-debian-buster-armv7:$VERSION
-docker push ghcr.io/qwandor/cross-dbus-debian-buster-x86_64:$VERSION
+docker push ghcr.io/qwandor/cross-dbus-debian-trixie-aarch64:$VERSION
+docker push ghcr.io/qwandor/cross-dbus-debian-trixie-armv7:$VERSION
+docker push ghcr.io/qwandor/cross-dbus-debian-trixie-armv6:$VERSION
+docker push ghcr.io/qwandor/cross-dbus-debian-trixie-x86_64:$VERSION
